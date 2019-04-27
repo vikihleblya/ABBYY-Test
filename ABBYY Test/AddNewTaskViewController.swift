079910a846d62ae76c02d442c4bb0faeaaad1c05
vikihleblya <vikihleblya@gmail.com>
2019-04-27 16:09:55 +0300

    Added "Main" and "AddTask" screens. Import IQKeyboardManager in pods.

---
+++ ABBYY Test/AddNewTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddNewTaskViewController.swift
+//  ABBYY Test
+//
+//  Created by Victor on 27/04/2019.
+//  Copyright © 2019 com.example.LoD. All rights reserved.
+//
+
+import UIKit
+
+class AddNewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = false
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ABBYY Test/MainTaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MainTaskTableViewCell.swift
+//  ABBYY Test
+//
+//  Created by Victor on 27/04/2019.
+//  Copyright © 2019 com.example.LoD. All rights reserved.
+//
+
+import UIKit
+
+class MainTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
